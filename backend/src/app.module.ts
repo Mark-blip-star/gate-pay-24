e3fd6676c58f242c3ead69a03470fe574f8d4e23
Ruslan Hutsul <ruslanhutsul343@gmail.com>
2026-01-28 17:59:01 +0200

    chore(backend): update app config, dependencies and e2e tests

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MeController } from './me/me.controller';
+import { PaymentsModule } from './payments/payments.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { StripeModule } from './stripe/stripe.module';
 import { TransactionsController } from './transactions/transactions.controller';
 
 @Module({
@@ -11,7 +14,10 @@ import { TransactionsController } from './transactions/transactions.controller';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    PrismaModule,
     AuthModule,
+    PaymentsModule,
+    StripeModule,
   ],
   controllers: [AppController, MeController, TransactionsController],
   providers: [AppService],
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,27 @@
-import { ValidationPipe } from '@nestjs/common';
+import { RequestMethod, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    rawBody: true, // Enable raw body for Stripe webhook
+  });
   app.enableCors({
     origin: true,
   });
-  app.setGlobalPrefix('api');
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  app.setGlobalPrefix('api', {
+    exclude: [
+      { path: 'pay', method: RequestMethod.GET },
+      { path: 'icons/*path', method: RequestMethod.GET },
+      { path: 'stripe/webhook', method: RequestMethod.POST },
+    ],
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
